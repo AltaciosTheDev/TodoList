2e88dff8f79958125a670095aa3a5084b779b38f
AltaciosTheDev <133562096+AltaciosTheDev@users.noreply.github.com>
2024-02-23 22:20:41 +0000

    Edit funcionality added

--- TodoList/src/Components/App.jsx
+++ TodoList/src/Components/App.jsx
@@ -5,7 +5,13 @@ import { nanoid } from "nanoid"
 function App() {
   const [task, setTask] = useState("")
   const [todos, setTodos] = useState([])
-  const [editMode, setEditMode] = useState(false)
+
+  //edit mode state will serve to indicate which function will be used in submit, either add or edit.
+  const [edit, setEdit] = useState({
+    mode:false,
+    id: "",
+    task: ""
+  })
 
   const inputRef = useRef()
 
@@ -15,36 +21,86 @@ function App() {
   },[todos])
 
   function handleSubmit(e){
+    //0.always prevent default so it doesnt load page again. 
     e.preventDefault()
 
-    if(/^\s*$/.test(task) === false){
-      if(editMode){
-        console.log("edit")
+    //1. if something written, call add or edit.
+    //    if(/^\s*$/.test(task) === false){}
+      //2. if edit mode, call edit, else call add.
+      if(edit.mode){
+        editTodo()
+        setEdit({
+          mode:false,
+          id: "",
+          task: ""
+        })
       }
       else{
         addTodo()
       }
-      
+      //3. after, task is emptied.
       setTask("")
-    }
-
-    else{
-      return 
-    }
 
   }
-  
+
+  function editTodo(){
+    let newTask = ""
+    if(/^\s*$/.test(task) === false){
+      newTask = task
+    }
+    else{
+      newTask = edit.task
+    }
+    let edittedTodo = {
+      id: edit.id,
+      task: newTask,
+      completed: false,
+      showDelete: false,
+      edit: false
+    }
+
+    setTodos(prevTodos => {
+      return prevTodos.map(prevTodo => {
+        return prevTodo.id === edit.id ? edittedTodo : prevTodo
+      })
+    })
+  }
+
+  //
+  function enterEditMode(id, task){
+    //1)Whichever todo was responsible, will switch edit to TRUE.
+    setTodos(prevTodos => {
+      return prevTodos.map(prevTodo => {
+          return prevTodo.id === id ? {...prevTodo, edit: true} : prevTodo
+      })
+    })
+
+    //2)any edit button will set the editMode = true
+    setEdit({
+      mode:true,
+      id: id,
+      task: task
+    })
+  }
+
 
   function addTodo(){
-    let newTodo = {
-      id: nanoid(),
-      task: task,
-      completed: false,
-      showDelete: false
+    if(/^\s*$/.test(task) === false){
+      let newTodo = {
+        id: nanoid(),
+        task: task,
+        completed: false,
+        showDelete: false,
+        edit: false
+      }
+      setTodos(prevTodos => {
+        return [newTodo, ...prevTodos]
+      })
     }
-    setTodos(prevTodos => {
-      return [newTodo, ...prevTodos]
-    })
+
+    else {
+      return
+    }
   }
 
   function handleDelete(id){
@@ -78,25 +134,28 @@ function App() {
                 <label htmlFor="item">New item</label>
                 <input ref={inputRef} id="item" value={task} onChange={(e) => setTask(e.target.value)}/>
             </div>
-            <button className="btn">Add</button>
+            <button className="btn">Submit</button>
         </form>
         <h1 className="header">Todo List</h1>
         <ul className="list">
 
           {todos.length === 0 && "Nothing todo... do something with your life!"}
 
-          {todos.map((todo) => {
+          {todos.map((todo) => { 
               return(
                   <li key={todo.id} className="itemWrapper" onMouseEnter={() => toggleShowDelete(todo.id)} onMouseLeave={() => toggleShowDelete(todo.id)}>
                     <label>
                         <input type="checkbox" checked={todo.completed} onChange={(e) => handleComplete(todo.id, e.target.checked)}/>
-                        {todo.task}
+                        {todo.edit? "Write corrections above and submit..." : todo.task }
                     </label>
-                    <div className="item-controls">
-                        {todo.showDelete && <button className="btn btn-danger" onClick={() => handleDelete(todo.id)}>Delete</button>}
-                        <button className="btn btn-warning" onClick={() => setEditMode(true)}>Modify</button>
-                    </div>
-                    
+                    {todo.edit ? 
+                      "" : 
+                      <div className="item-controls">
+                          {todo.showDelete && <button className="btn btn-danger" onClick={() => handleDelete(todo.id)}>Delete</button>}
+                          {edit.mode === false && <button className="btn btn-warning" onClick={() => enterEditMode(todo.id, todo.task)}>Modify</button>}
+                      </div>
+                    }
+
                   </li>
               )
           })}
